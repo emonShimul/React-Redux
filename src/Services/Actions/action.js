aa1881b45488a137dce62a3d093ff3b41b835c81
emoncse53 <emoncse53@gmail.com>
2021-06-23 00:03:52 +0600

    redux reducer and root reducer code is written in the add to cart design page

--- src/Services/Actions/action.js
+++ src/Services/Actions/action.js
@@ -1,8 +1,8 @@
-import {ADD_TO_CART} from '../constants'
+import { ADD_TO_CART } from '../constants'
 
 export const addToCart = (data) => {
     return {
-        type:ADD_TO_CART,
-        data:data
+        type: ADD_TO_CART,
+        data: data
     }
 }
