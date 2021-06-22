8d33183f90d2207eb52e7ea916bfdcf3b6f0b9b2
emoncse53 <emoncse53@gmail.com>
2021-06-23 00:30:45 +0600

    Added redux container code using mapStateToProps and mapDispatchToProps functions to connect react with redux

--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -1,3 +1,16 @@
+import {connect} from 'react-redux'
 import Home from "../Components/Home";
+import {addToCart} from '../Services/Actions/action'
 
-export default Home;
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+    addToCartHandler:data => dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
+
+
+// export default Home;
